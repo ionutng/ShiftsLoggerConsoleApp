d01bded097bfa397aca368625b94c6bfc88a4b9a
ionutng <ionutngp@gmail.com>
2023-12-25 14:52:47 +0200

    Add try-catch blocks around the API calls to handle unexpected errors

--- ShiftsLoggerConsoleApp/ShiftsService.cs
+++ ShiftsLoggerConsoleApp/ShiftsService.cs
@@ -8,66 +8,108 @@ internal static class ShiftsService
 {
     internal static List<Shift> GetShifts()
     {
-        var client = new RestClient("https://localhost:7290/");
-        var request = new RestRequest("api/Shifts");
-        var response = client.Get(request);
+        try
+        {
+            var client = new RestClient("https://localhost:7290/");
+            var request = new RestRequest("api/Shifts");
+            var response = client.Get(request);
 
-        List<Shift> shifts = JsonConvert.DeserializeObject<List<Shift>>(response.Content);
+            List<Shift> shifts = JsonConvert.DeserializeObject<List<Shift>>(response.Content);
 
-        UserInterface.ShowShifts(shifts);
+            UserInterface.ShowShifts(shifts);
 
-        return shifts;
+            return shifts;
+        } catch (Exception ex)
+        {
+            Console.Clear();
+            Console.WriteLine(ex.Message + "\n");
+            UserInterface.Menu();
+        }
+
+        return [];
     }
 
     internal static void GetShift()
     {
-        string shiftId = ShiftsHelper.GetShiftId();
+        try
+        {
+            string shiftId = ShiftsHelper.GetShiftId();
 
-        var client = new RestClient("https://localhost:7290/");
-        var request = new RestRequest($"api/Shifts/{HttpUtility.UrlEncode(shiftId)}");
-        var response = client.Get(request);
+            var client = new RestClient("https://localhost:7290/");
+            var request = new RestRequest($"api/Shifts/{HttpUtility.UrlEncode(shiftId)}");
+            var response = client.Get(request);
 
-        Shift shift = JsonConvert.DeserializeObject<Shift>(response.Content);
+            Shift shift = JsonConvert.DeserializeObject<Shift>(response.Content);
 
-        UserInterface.ShowShift(shift);
+            UserInterface.ShowShift(shift);
+        } catch (Exception ex)
+        {
+            Console.Clear();
+            Console.WriteLine(ex.Message + "\n");
+            UserInterface.Menu();
+        }
     }
 
     internal static void AddShift()
     {
-        var shift = ShiftsHelper.GetShiftInput();
+        try
+        {
+            var shift = ShiftsHelper.GetShiftInput();
 
-        var client = new RestClient("https://localhost:7290/");
-        var request = new RestRequest("api/Shifts");
+            var client = new RestClient("https://localhost:7290/");
+            var request = new RestRequest("api/Shifts");
 
-        request.AddJsonBody(shift);
+            request.AddJsonBody(shift);
 
-        var response = client.Post(request);
+            var response = client.Post(request);
+        } catch (Exception ex)
+        {
+            Console.Clear();
+            Console.WriteLine(ex.Message + "\n");
+            UserInterface.Menu();
+        }
     }
 
     internal static void UpdateShift()
     {
-        string shiftId = ShiftsHelper.GetShiftId();
+        try
+        {
+            string shiftId = ShiftsHelper.GetShiftId();
 
-        var client = new RestClient("https://localhost:7290/");
-        var request = new RestRequest($"api/Shifts/{HttpUtility.UrlEncode(shiftId)}");
-        var response = client.Get(request);
+            var client = new RestClient("https://localhost:7290/");
+            var request = new RestRequest($"api/Shifts/{HttpUtility.UrlEncode(shiftId)}");
+            var response = client.Get(request);
 
-        Shift shift = JsonConvert.DeserializeObject<Shift>(response.Content);
+            Shift shift = JsonConvert.DeserializeObject<Shift>(response.Content);
 
-        shift = ShiftsHelper.UpdateShift(shift);
+            shift = ShiftsHelper.UpdateShift(shift);
 
-        request.AddJsonBody(shift);
+            request.AddJsonBody(shift);
 
-        var response2 = client.ExecutePut(request);
+            var response2 = client.ExecutePut(request);
+        } catch (Exception ex)
+        {
+            Console.Clear();
+            Console.WriteLine(ex.Message + "\n");
+            UserInterface.Menu();
+        }
     }
 
     internal static void DeleteShift()
     {
-        string shiftId = ShiftsHelper.GetShiftId();
+        try
+        {
+            string shiftId = ShiftsHelper.GetShiftId();
 
-        var client = new RestClient("https://localhost:7290/");
-        var request = new RestRequest($"api/Shifts/{HttpUtility.UrlEncode(shiftId)}");
+            var client = new RestClient("https://localhost:7290/");
+            var request = new RestRequest($"api/Shifts/{HttpUtility.UrlEncode(shiftId)}");
 
-        var response = client.Delete(request);
+            var response = client.Delete(request);
+        } catch (Exception ex)
+        {
+            Console.Clear();
+            Console.WriteLine(ex.Message + "\n");
+            UserInterface.Menu();
+        }
     }
 }
